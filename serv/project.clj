27e244ed9b5cb9ce255dc84c1c33b07d79e35119
Dan Donley <ddonley97@gmail.com>
2023-02-22 11:04:54 -0500

    [#183997533] PR comments; fixing spacing and absent newlines

--- serv/project.clj
+++ serv/project.clj
@@ -19,3 +19,4 @@
             :ring {:handler serv.handler/app}
             :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                             [ring/ring-mock "0.3.2"]]}})
+
