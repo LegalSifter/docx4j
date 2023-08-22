469ce21d3bd53b9f60a388beb1d9a49d9ba91ef0
Dan Donley <ddonley97@gmail.com>
2023-08-22 16:30:30 -0400

    [#184540251] Regex Denial of Service (ReDoS) (3)

--- xsd/license_files/loadImg.js
+++ xsd/license_files/loadImg.js
@@ -46,18 +46,31 @@ function PrintThisPage()
 
 if (ptcl == "http:"){
 		oldTerm= splitted[2];
-		newTerm= "/"+oldTerm + "/flat";
-		url=replace2FlatUrl(loc, oldTerm, newTerm)
- 		PopMeThis(url,750, 600,100,25)
+		// oldterm is used directly in a regular expression, so it needs escaped
+		if (oldTerm == escapeRegexCharacters(oldTerm)) // don't allow regex characters
+		{
+			newTerm= "/"+oldTerm + "/flat";
+			url=replace2FlatUrl(loc, oldTerm, newTerm);
+			PopMeThis(url,750, 600,100,25);
+		}
 } 
 else
 {
 		loc=window.location.href.toLowerCase().split("//");
 		oldTerm = loc[1].split("/")[2];
-		newTerm = "/flat/"+oldTerm
-		url=replace2FlatUrl(window.location.href, oldTerm, newTerm)
-		PopMeThis(url,750,600,100,25)
+		// oldterm is used directly in a regular expression, so it needs escaped
+		if (oldTerm === escapeRegexCharacters(oldTerm)) // don't allow regex characters
+		{
+			newTerm = "/flat/"+oldTerm
+			url=replace2FlatUrl(window.location.href, oldTerm, newTerm)
+			PopMeThis(url,750,600,100,25)
+		}
 }
+}
+
+// escape regex characters: https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
+function escapeRegexCharacters(string) {
+    return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
 //  build pop-up window
